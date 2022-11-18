899100e9418eba586115d0626f1f9ccb980912d4
leylosko9 <107769019+leylosko9@users.noreply.github.com>
2022-11-18 23:54:50 +0300

    Add files via upload

---
+++ rarity-score-main/config/index.js
@@ -0,0 +1,9 @@
+export const config = {
+  env: "local",
+  LOCAL_API_URL: "rarity-score-skratz.vercel.app",
+  API_URL: "rarity-score-skratz.vercel.app",
+  COLLECTION_NAME: "House of Skratz",
+  COLLECTION_TITLE: "House of Skratz",
+  COLLECTION_DESCRIPTION: '',
+  COLLECTION_IMG_LINK: "https://pbs.twimg.com/profile_images/1460282903393300480/A4den89y_400x400.jpg",
+};
